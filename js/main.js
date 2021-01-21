a5d4cbb01b22fe74a92abd3c4204d27f2f251a9a
Haniel <hhernandez@aimservices.tech>
2021-01-20 18:05:11 -0600

    Modal sizes added and modal triggers improved

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("click", function ($event) {
+
+    const parent =  $event.target.parentNode;
     if (
         $event.target.classList.contains("nav-toggler") ||
         $event.target.classList.contains("nav-toggler-icon")
@@ -15,6 +17,15 @@ document.addEventListener("click", function ($event) {
             menu.style.maxHeight = content.scrollHeight + "px";
         }
     }
+    // if click on element with .modal-trigger open modal
+    if($event.target.classList.contains('modal-trigger')){
+        showModal($event.target);
+    }else{
+        if(parent.classList.contains('modal-trigger')) {
+            showModal(parent);
+        }
+    }
+
 });
  
 function expand(){
@@ -46,12 +57,13 @@ function expandSidebar(){
         sidebar.classList.add('expanded');
     }
 }
+
 const expandTrigger =  document.getElementById('expandTrigger');
 expandTrigger.addEventListener('click',expandSidebar)
 
-function showModal($event){
-    const target = document.getElementById($event.target.dataset.target);
-    console.log(target.style.display)
+function showModal(trigger){
+    const target = document.getElementById(trigger.dataset.target);
+    console.log(target)
     if(target.style.display =="none" || target.style.display === ''){
         target.style.display = "block";
         setTimeout(()=>{
@@ -69,7 +81,10 @@ function showModal($event){
         }
     }
 }
+
+/*
 const modalTriggers =  document.getElementsByClassName('modal-trigger');
+
 for (var i = 0; i < modalTriggers.length; i++) {
     modalTriggers[i].addEventListener('click', showModal);
-}
+}*/
